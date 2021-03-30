d022a0864429ed78e82780e11ce6ddda843cc4d0
Elizaveta Frolova <elizaveta.fr@gmail.com>
2021-03-30 14:22:49 +0300

    corrected Task2 Day4

--- src/main/java/day4/Task2.java
+++ src/main/java/day4/Task2.java
@@ -16,27 +16,24 @@ public class Task2 {
         for (int i = 0; i < 100; i++) {
             nums[i] = (int) (Math.random() * 10000);
         }
-            int max = 0;
-            int min = 10000;
-            for (int num : nums) {
-                if (num >= max) {
-                    max = num;
-                }
-            }
-            for (int num : nums) {
-                if (num <= min) {
-                    min = num;
-                }
-            }
 
-            int count_0 = 0;
-            int sum_0 = 0;
-            for (int num : nums) {
-                if (num % 10 == 0) {
-                    count_0 += 1;
-                    sum_0 += num;
-                }
+
+        int max =nums[0];
+        int min =nums[0];
+
+        int count_0 = 0;
+        int sum_0 = 0;
+
+        for (int num : nums) {
+            if (num >= max) {
+                max = num;
+            } else if (num < min) {
+                min = num;
+            } else if (num % 10 == 0) {
+                count_0 += 1;
+                sum_0 += num;
             }
+        }
 
         System.out.println(max);
         System.out.println(min);
